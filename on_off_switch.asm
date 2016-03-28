a8c6e04106da0bd2363eb2da9833a59bc23a966f
Hernán Eche <hernan_eche@yahoo.com.ar>
2016-03-28 15:07:37 -0300

    Update on_off_switch.asm

--- on_off_switch.asm
+++ on_off_switch.asm
@@ -5,18 +5,18 @@ TRISB 	equ 0x86
 		org 0x00		 
 
 		bsf STATUS,5	; banco 1
-		bcf TRISB,0		; configura RB0 como salida
-		bsf TRISB,1		; configura RB1 como entrada
+		bcf TRISB,0	; configura RB0 como salida
+		bsf TRISB,1	; configura RB1 como entrada
 		bcf STATUS,5	; banco 0
 
-		bcf PORTB,0		;comienza led apagado
+		bcf PORTB,0	;comienza led apagado
 		
 bucle:		btfss PORTB,1	;espera switch on
 		goto bucle
-		bsf PORTB,0		;encendido
+		bsf PORTB,0	;encendido
 bucle2:		btfsc PORTB,1 	;espera switch off
 		goto bucle2
-		bcf PORTB,0		;apagar
+		bcf PORTB,0	;apagar
 		goto bucle
 		
 		end
