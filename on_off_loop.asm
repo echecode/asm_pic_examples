e1f653d9e7a19bf900987246a37c289a4a400cbd
Hernán Eche <hernan_eche@yahoo.com.ar>
2016-03-28 15:32:23 -0300

    Update on_off_loop.asm

--- on_off_loop.asm
+++ on_off_loop.asm
@@ -1,8 +1,8 @@
-			org 0x00		; inicia programa en dirección 0
-inicio:			nop				
-			bsf 0x20,4		; pone en 1 bit 4 del registro 0x20
-			nop				
-			nop				
-			bcf 0x20,4		; pone en 0 bit 4 del registro 0x20
-			goto inicio		
-			end			; final 
+	org 0x00		; inicia programa en dirección 0
+inicio:	nop				
+	bsf 0x20,4		; pone en 1 bit 4 del registro 0x20
+	nop				
+	nop				
+	bcf 0x20,4		; pone en 0 bit 4 del registro 0x20
+	goto inicio		
+	end			; final 
